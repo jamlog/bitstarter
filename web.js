29ac1e9aad2967e99543c810b136b12e59768f06
Brian Ruff <locomojo@gmail.com>
2013-07-12 05:07:13 +0000

    added fs.readFileSync to grab html file

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+	var myfile = fs.readFileSync('./index.html','utf8');
+	response.send(myfile);
+
+
 });
 
 var port = process.env.PORT || 5000;
